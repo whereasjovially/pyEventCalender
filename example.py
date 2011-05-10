3b6ae1b005b068c86013f1858cd725c0698861b1
whereasjovially <whereasjovially@outlook.com>
2011-05-10 20:56:54 +0100

    Working example

--- example.py
+++ example.py
@@ -1,8 +1,14 @@
+import sys
 from pyeventcalendar import Event, EventCalendar
 from datetime import date
+import calendar
 
-def __main__():
-  cal = EventCalendar()
-  cal.addevent(Event(start=date(2011, 3, 4), end=date(2011, 3, 6), title="Rome"))
-  cal.addevent(Event(start=date(2011, 3, 4), end=date(2011, 3, 6), title="Paris"))
+def main():
+  cal = EventCalendar(calendar.MONDAY, rows=3)
+  cal.addevent(Event(pk=1, start=date(2011, 3, 3), end=date(2011, 3, 6), title="Rome"))
+  cal.addevent(Event(pk=2, start=date(2011, 3, 4), end=date(2011, 3, 14), title="Paris"))
+  cal.addevent(Event(pk=3, start=date(2011, 3, 10), end=date(2011, 3, 16), title="Venice"))
   print cal.formatmonth(2011, 3)
+
+if __name__ == "__main__":
+  sys.exit(main())
