650fdbb52c6a4a87f8dda28f58b8cdbb0cbbda1a
whereasjovially <whereasjovially@outlook.com>
2011-05-13 11:25:04 +0200

    Organise files as per http://as.ynchrono.us/2007/12/filesystem-structure-of-python-project_21.html

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[],
+    py_modules=['pyeventcalendar'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
